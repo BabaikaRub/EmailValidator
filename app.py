69ca43643231fa8570978b41ba9b6ed45f2355a7
BabaikaRub <rodionov.mihail02@mail.ru>
2022-08-10 22:24:55 +0300

    change popup message, add thread for email_validator

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import traceback
 import sys
 
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QObject, pyqtSignal, QThread
 from PyQt5.QtWidgets import QMessageBox, QFileDialog
+from openpyxl.utils.exceptions import InvalidFileException
 
 from ui import Ui_MainWindow
 from validator import EmailValidator
@@ -20,33 +22,57 @@ class App(QtWidgets.QMainWindow):
 
     def load_file(self):
         dialog = QFileDialog.getOpenFileName(self, "Select Excel file to import", "", "Excel (*.xls *.xlsx)")
+        file_name = dialog[0]
 
-        if dialog:
-            self.ui.label_2.setText('Файл загружен.')
+        self.start_validate(file_name)
 
-            file_name = dialog[0]
+    @staticmethod
+    def popup_info():
+        msg = QMessageBox()
+        msg.setWindowTitle('Информация')
+        msg.setText('Для начала работы выберите файл!')
 
-            self.start_validate(file_name)
+        x = msg.exec_()
+
+    def show_end(self):
+        self.ui.label_2.setText('Файл записан')
+        self.ui.pushButton.setEnabled(1)
 
     def init_ui(self):
         self.setWindowTitle('EmailValidator')
 
-    def popup_end(self):
-        msg = QMessageBox()
-        msg.setWindowTitle('Информация')
-        msg.setText('Файл успешно записан!!!')
-
-        x = msg.exec_()
-
     def start_validate(self, file_name):
-        email_validator = EmailValidator(file_name)
+        self.obj = Validator(file_name)
+        self.t = QThread()
+        self.obj.moveToThread(self.t)
+        self.t.started.connect(self.obj.start)
+        self.obj.finishSignal.connect(self.t.quit)
+        self.obj.finishSignal.connect(self.show_end)
 
-        addresses = email_validator.get_info()
-        check_result = email_validator.check_address(addresses)
-        flag = email_validator.write_data(check_result)
+        self.t.start()
 
-        if flag:
-            self.popup_end()
+        self.ui.pushButton.setEnabled(0)
+
+
+class Validator(QObject):
+    finishSignal = pyqtSignal()
+
+    def __init__(self, file_name):
+        super().__init__()
+        self.file_name = file_name
+
+    def start(self):
+        try:
+            email_validator = EmailValidator(self.file_name)
+
+            addresses = email_validator.get_info()
+            check_result = email_validator.check_address(addresses)
+            email_validator.write_data(check_result)
+
+            self.finishSignal.emit()
+
+        except InvalidFileException:
+            App.popup_info()
 
 
 def main():
